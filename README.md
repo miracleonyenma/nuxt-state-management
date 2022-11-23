eebbd6fb5e2d21d08b1673b8fdbb6610725124e1
miracleonyenma <42788603+miracleonyenma@users.noreply.github.com>
2022-11-23 08:30:01 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nuxt-state-management
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nuxt-starter-ffu62p)
---
+++ app.vue
@@ -0,0 +1,12 @@
+<script setup>
+const state = ref({
+  count: 0,
+});
+
+const incrementCount = () => state.value.count++;
+</script>
+<template>
+  <div>
+    <button @click="incrementCount">Clicked {{ state.count }} times</button>
+  </div>
+</template>
